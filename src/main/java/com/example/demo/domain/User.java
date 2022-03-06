75e281b72b45bf4ce3851d0b78c57b4c6364cfd3
zhuchunyou <zhuchunyou@126.com>
2022-03-06 22:11:23 +0800

    first

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,8 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = -2731598327208972274L;
+
+}
